75fe15b6592a26aa3191a9bf3ec6fa11e3868528
ogilcher <olivergilcher@gmail.com>
2025-02-16 00:07:37 -0800

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Oliver Gilcher on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
